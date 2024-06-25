ff488ce99b39d85716a691a693bd91a9ba6ae584
jjhj22 <skarnstktn@naver.com>
2024-06-25 18:58:42 +0900

    아아

--- 20240624/stactic/javascript/game.js
+++ 20240624/stactic/javascript/game.js
@@ -9,9 +9,9 @@ function timeStart(){
 }
 function inClick(){
     var idx= $(".item").index($(this));  //몇번째 div 클릭했냐? 인덱스
-    var clickIng=$(".item").eq(idx).find("img"); //클릭한 div의 img태그
+    var clickImg=$(".item").eq(idx).find("img"); //클릭한 div의 img태그
 
     //클릭한 이미지 화면에 표시
     $clickImg.removeClass("hide");
-    $clickIng.addClass("select");
+    $clickImg.addClass("select");
 }
